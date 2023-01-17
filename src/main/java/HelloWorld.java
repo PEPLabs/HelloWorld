d98a12ae3ad867f57c92a67c88d824c8b191b041
tedbeast <theodore.balashov@revature.com>
2023-01-17 12:17:59 -0500

    added clarifying comments to helloworld lab

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,15 @@
-
 public class HelloWorld {
     /**
-     * This method must return a hello world String, instead of returning "change me".
-     * A test case will check if the behavior is as expected.
-     * You can use the test results to check for differences.
+     * This method currently contains the line:
+     * return "change me";
+     * That means that this method is a piece of code that produces a String "change me", 
+     * and when this method is used in other parts of my program, the value "change me"
+     * is returned.
+     * 
+     * Instead, this method must return a hello world String, instead of returning 
+     * "change me". A test case will check if the behavior is as expected.
+     * You should use the test results to check for differences.
+     * 
      * @return "Hello, world!"
      */
     public String sayHello(){
